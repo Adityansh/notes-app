0533756f3c0f38fa26f375196c9d3faa6e89f6c0
Adityansh Sharma <sharmaadityansh@gmail.com>
2021-12-14 21:57:11 +0530

    hi

--- app.js
+++ app.js
@@ -54,11 +54,3 @@ yargs.command({
 });
 
 yargs.parse();
-
-// const command = process.argv[2];
-
-// if (command == "add") {
-//   console.log("Adding note!");
-// } else if (command == "remove") {
-//   console.log("Removing notes!");
-// }
